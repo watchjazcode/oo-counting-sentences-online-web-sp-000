b7f865a624eef12bff249837b607e15622176531
Jazmin M <jazminmerckel@gmail.com>
2019-10-23 23:57:25 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,6 +18,7 @@ class String
   def count_sentences
     # Split the string into multiple sentences based on punctuation
     multiple_sents = self.split(/[\.\!\?]/)
+    
     non_empty_sents = multiple_sents.reject {|sentence| sentence == ""}
     count = non_empty_sents.length
     
